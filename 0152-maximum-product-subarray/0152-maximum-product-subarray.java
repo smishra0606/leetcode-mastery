0d4b9ba68ae2b6879f12d74a0d8aa1dc2663db58
Shiven Mishra <shiven.mishra23@lpu.in>
2026-08-17 22:53:53 +0530

    Time: 2 ms (72.04%), Space: 47.1 MB (88.76%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+        int max=nums[0];
+        int min=nums[0];
+        int result=nums[0];
+
+        for (int i=1;i<nums.length;i++){
+            int curr=nums[i];
+            if (curr<0){
+                int temp=max;
+                max=min;
+                min=temp;
+            }
+            max=Math.max(curr,max*curr);
+            min= Math.min(curr,min*curr);
+            result=Math.max(result,max);
+        }
+        return result;
+    }
+}
